769ef742995e298cd14b7c382a855f335f3ee286
VMubuntu <vm@ubuntu.com>
2022-07-05 18:45:11 +0200

    7th commit

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: retcheba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 16:53:44 by retcheba          #+#    #+#             */
-/*   Updated: 2022/07/04 17:27:47 by retcheba         ###   ########.fr       */
+/*   Updated: 2022/07/05 18:32:24 by retcheba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 int	ft_close(t_game *game)
 {
-	mlx_destroy_image(game->mlx, game->img);
+	mlx_destroy_image(game->mlx, game->player);
+	mlx_destroy_image(game->mlx, game->grass);
 	mlx_destroy_window(game->mlx, game->win);
 	mlx_destroy_display(game->mlx);
 	free(game->mlx);
@@ -26,6 +27,30 @@ int	keypress(int keycode, t_game *game)
 {
 	if (keycode == 65307)
 		ft_close(game);
+	if (keycode == 100 || keycode == 65363)
+	{
+		mlx_put_image_to_window(game->mlx, game->win, game->grass, game->x, game->y);
+		game->x += 48;
+		mlx_put_image_to_window(game->mlx, game->win, game->player, game->x, game->y);
+	}
+	if (keycode == 113 || keycode == 65361)
+	{
+		mlx_put_image_to_window(game->mlx, game->win, game->grass, game->x, game->y);
+		game->x -= 48;
+		mlx_put_image_to_window(game->mlx, game->win, game->player, game->x, game->y);
+	}
+	if (keycode == 122 || keycode == 65362)
+	{
+		mlx_put_image_to_window(game->mlx, game->win, game->grass, game->x, game->y);
+		game->y -= 48;
+		mlx_put_image_to_window(game->mlx, game->win, game->player, game->x, game->y);
+	}
+	if (keycode == 115 || keycode == 65364)
+	{
+		mlx_put_image_to_window(game->mlx, game->win, game->grass, game->x, game->y);
+		game->y += 48;
+		mlx_put_image_to_window(game->mlx, game->win, game->player, game->x, game->y);
+	}
 	return (0);
 }
 
@@ -34,10 +59,13 @@ int	main(void)
 	t_game	game;
 
 	game.mlx = mlx_init();
-	game.win = mlx_new_window(game.mlx, 541, 524, "so_long");
-	game.img = mlx_xpm_file_to_image(game.mlx, "planet.xpm", &game.img_width, &game.img_height);
+	game.win = mlx_new_window(game.mlx, 1152, 720, "so_long");
+	game.grass = mlx_xpm_file_to_image(game.mlx, "sprites/grass.xpm", &game.img_width, &game.img_height);
+	game.player = mlx_xpm_file_to_image(game.mlx, "sprites/player.xpm", &game.img_width, &game.img_height);
 	mlx_key_hook(game.win, keypress, &game);
 	mlx_hook(game.win, 17, 0, ft_close, &game);
-	mlx_put_image_to_window(game.mlx, game.win, game.img, 0, 0);
+	game.x = 0;
+	game.y = 0;
+	mlx_put_image_to_window(game.mlx, game.win, game.player, game.x, game.y);
 	mlx_loop(game.mlx);
 }
--- so_long.h
+++ so_long.h
@@ -6,24 +6,27 @@
 /*   By: retcheba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 11:54:38 by retcheba          #+#    #+#             */
-/*   Updated: 2022/07/04 12:17:33 by retcheba         ###   ########.fr       */
+/*   Updated: 2022/07/05 17:44:39 by retcheba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-#include "mlx.h"
-#include <stdlib.h>
-#include <stdio.h>
+# include "mlx.h"
+# include <stdlib.h>
+# include <stdio.h>
 
 typedef struct s_game
 {
 	void	*mlx;
 	void	*win;
-	void	*img;
+	void	*player;
+	void	*grass;
 	int		img_width;
 	int		img_height;
+	int		x;
+	int		y;
 }	t_game;
 
 int	ft_close(t_game *game);
